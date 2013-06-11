0112bd2d9820c9ed7c707622eccc889b4a03d523
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2013-06-11 21:08:56 +0300

    Added dynamic find_by_* method

--- lib/devise/oauth2_providable/base.rb
+++ lib/devise/oauth2_providable/base.rb
@@ -2,4 +2,18 @@ class Devise::Oauth2Providable::Base
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::Validations
+
+  class << self
+    def method_missing(method, *args)
+      method.to_s.match(/^find_by_(\w+)$/) do |m|
+        attribute = m[1].to_sym
+        return where(attribute => args.first).first
+      end
+      super
+    end
+
+    def respond_to?(method, *args)
+      method.to_s.starts_with?('find_by_') or super
+    end
+  end
 end
